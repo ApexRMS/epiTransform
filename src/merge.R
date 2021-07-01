9a2797f432bdc229922dcbddf4f0b8b13499ebfd
Shreeram Senthivasan <shreeramsenthi@gmail.com>
2021-07-01 08:14:18 -0700

    Choose how to handle source transformer data

--- src/merge.R
+++ src/merge.R
@@ -1,5 +1,22 @@
 library(rsyncrosim)
+library(dplyr)
+library(stringr)
 
-# Read in merged inputs and write back out
-dataSummary <- datasheet(scenario(), "epi_DataSummary", optional = T, lookupsAsFactors = F)
-saveDatasheet(scenario(), dataSummary, "epi_DataSummary")
+# Read in run settings
+settings <- datasheet(scenario(), "epiTransform_MergeInputs")
+
+transformerName <- "Data Transformations: Merge Scenarios"
+
+# Read in merged inputs
+originalData <- datasheet(scenario(), "epi_DataSummary", optional = T, lookupsAsFactors = F)
+
+# Choose if or how to modify the source transformer data
+if(str_detect(settings$Keep, "Keep both")){
+  saveDatasheet(scenario(), bind_rows(originalData, originalData %>% mutate(TransformerID = transformerName)), "epi_DataSummary")
+  
+} else if(str_detect(settings$Keep, "Only keep original")){
+  saveDatasheet(scenario(), originalData, "epi_DataSummary")
+  
+} else # Only keep consolidated
+  saveDatasheet(scenario(), originalData %>% mutate(TransformerID = transformerName), "epi_DataSummary")
+
